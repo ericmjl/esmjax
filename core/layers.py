57755d8e20a7c75e4bc8aeb4ce8b68adb585db27
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-09-29 11:26:06 +0000

    add tokenization + masking

--- core/layers.py
+++ core/layers.py
@@ -5,29 +5,35 @@ import jax.numpy as jnp
 import jax
 
 class SelfMultiHeadAttention(hk.MultiHeadAttention):
-    """Self attention with no mask applied"""
+    """Self attention with mask applied"""
     
     def __call__(
         self, 
-        x: jnp.ndarray) -> jnp.ndarray:
+        x: jnp.ndarray,
+        att_mask: Optional[jnp.ndarray] = None) -> jnp.ndarray:
 
-        return super().__call__(query=x, key=x, value=x)
+        return super().__call__(query=x, key=x, value=x, mask=att_mask)
 
 
 class TransformerLayer(hk.Module):
-    def __init__(self, num_heads:int, key_size:int, name: Optional[str]=None):
+    def __init__(self, 
+                num_heads:int, 
+                key_size:int, 
+                name: Optional[str]=None):
+
         super().__init__(name=name)
         self.num_heads = num_heads
         self.key_size = key_size
     
     def __call__(
         self, 
-        x: jnp.ndarray) -> jnp.ndarray:
+        x: jnp.ndarray,
+        att_mask: Optional[jnp.ndarray] = None) -> jnp.ndarray:
 
         res = x 
 
         x = hk.LayerNorm(axis=-1, create_scale=True, create_offset=True, name="self_attn_layer_norm")(x)
-        x = SelfMultiHeadAttention(self.num_heads, self.key_size, 1)(x)
+        x = SelfMultiHeadAttention(self.num_heads, self.key_size, 1)(x, att_mask)
         x = res + x 
 
         res = x 
@@ -37,3 +43,40 @@ class TransformerLayer(hk.Module):
         x = res + x
 
         return x
+
+class LearnedPositionalEmbeddings(hk.Module):
+    def __init__(self, 
+                vocab_size: int,
+                embed_dim: int,
+                padding_idx: Optional[int] = 1,
+                name: Optional[str] = None):
+
+        super().__init__(name=name)
+        self.vocab_size = vocab_size
+        self.embed_dim = embed_dim
+        self.padding_idx = padding_idx
+    
+    def __call__(self, tokens: jnp.ndarray) -> jnp.ndarray:
+        mask = tokens != self.padding_idx
+        positions = jnp.cumsum(mask, axis=1) * mask + self.padding_idx
+
+        return hk.Embed(self.vocab_size + self.padding_idx + 1, self.embed_dim)(positions)
+
+class ESM1b(hk.Module):
+    def __init__(self,
+                padding_idx: Optional[int] = 1,
+                name: Optional[str] = None):
+        
+        self.padding_idx = padding_idx
+        super().__init__(name=name)
+    
+    def __call__(self, tokens: jnp.ndarray) -> jnp.ndarray:
+        padding_mask = tokens == self.padding_idx
+
+        att_mask = ~padding_mask[:, None, :]
+        att_mask = jnp.einsum("bhT, bht->bhtT", att_mask, att_mask)
+
+        x = hk.Embed(33, 1280)(tokens)
+        x = x + LearnedPositionalEmbeddings(1024, 1280)(tokens)
+
+        return x, att_mask
