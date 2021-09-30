07ce3ac128c3ba8dc20286ef8c8f1cec67a8569f
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-09-30 03:19:48 +0000

    restructure attention

--- core/layers.py
+++ core/layers.py
@@ -11,8 +11,8 @@ import numpy as np
 # Class is modified from MultiHeadAttention here
 # https://github.com/deepmind/dm-haiku/blob/main/haiku/_src/attention.py
 # Full original Apache 2.0 license here https://github.com/deepmind/dm-haiku/blob/main/LICENSE
-class SelfMultiHeadAttention(hk.MultiHeadAttention):
-    """Self attention with mask applied. Modified from 
+class SelfMHA(hk.MultiHeadAttention):
+    """Self multi-head attention with mask applied. Modified from 
     original implementation to be able to return attention
     weights (needed for contact prediction)"""
     
@@ -40,9 +40,18 @@ class SelfMultiHeadAttention(hk.MultiHeadAttention):
         x: jnp.ndarray,
         mask: Optional[jnp.ndarray] = None,
     ) -> jnp.ndarray:
+
+        attn_weights = self.attn_weights(x, mask)
+        return self.compute_tokens(x, attn_weights)
+        
+    @hk.transparent
+    def attn_weights(
+        self, 
+        x: jnp.ndarray, 
+        mask: Optional[jnp.ndarray] = None,
+        ) -> jnp.ndarray:
         query_heads = self._linear_projection(x, self.key_size, "query")
         key_heads = self._linear_projection(x, self.key_size, "key")
-        value_heads = self._linear_projection(x, self.value_size, "value")
 
         attn_logits = jnp.einsum("...thd,...Thd->...htT", query_heads, key_heads)
         sqrt_key_size = np.sqrt(self.key_size).astype(x.dtype)
@@ -53,28 +62,33 @@ class SelfMultiHeadAttention(hk.MultiHeadAttention):
             attn_logits = jnp.where(mask, attn_logits, -1e30)
 
         attn_weights = jax.nn.softmax(attn_logits)
+
+        return attn_weights
+    
+    @hk.transparent
+    def compute_tokens(
+        self,
+        x: jnp.ndarray,
+        attn_weights: jnp.ndarray,
+    ) -> jnp.ndarray:
+        value_heads = self._linear_projection(x, self.value_size, "value")
         attn = jnp.einsum("...htT,...Thd->...thd", attn_weights, value_heads)
         
         # Concatenate attention matrix of all heads into a single vector.
         attn_vec = jnp.reshape(attn, (*x.shape[:-1], -1))
-        x = hk.Linear(self.model_size, w_init=self.w_init)(attn_vec)
-        
-        # Return attention weights if requested at initialization
-        if self.head_weights:
-            return x, attn_weights
-        else:
-            return x
-
+        return hk.Linear(self.model_size, w_init=self.w_init)(attn_vec)
 
 class TransformerLayer(hk.Module):
     """Each of the blocks that form the 33-layer tower"""
     def __init__(self, 
                 num_heads:int, 
                 key_size:int, 
+                head_weights: Optional[bool] = False,
                 name: Optional[str]=None):
 
         super().__init__(name=name)
         self.num_heads = num_heads
+        self.head_weights = head_weights
         self.key_size = key_size
     
     def __call__(
@@ -86,7 +100,8 @@ class TransformerLayer(hk.Module):
         res = x 
 
         x = hk.LayerNorm(axis=-1, create_scale=True, create_offset=True, name="self_attn_layer_norm")(x)
-        x = SelfMultiHeadAttention(self.num_heads, self.key_size, 1)(x, att_mask)
+        x = SelfMHA(self.num_heads, self.key_size, 1, self.head_weights)(x, att_mask)
+
         x = res + x 
 
         res = x 
