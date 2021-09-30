f9d0a1e401d4665b693548e0338c0c1015a683e5
Irhum Shafkat <13271155+irhum@users.noreply.github.com>
2021-09-30 12:37:35 +0000

    add contact pred head

--- core/layers.py
+++ core/layers.py
@@ -1,7 +1,5 @@
 from typing import Optional, Tuple
 
-from jax.interpreters.xla import xla_primitive_callable
-
 import haiku as hk
 import jax
 import jax.numpy as jnp
@@ -63,9 +61,9 @@ class SelfMHA(hk.MultiHeadAttention):
 class TransformerLayer(hk.Module):
     """Each of the blocks that form the 33-layer tower"""
     def __init__(self, 
-                num_heads:int, 
-                key_size:int, 
-                name: Optional[str]=None):
+        num_heads:int, 
+        key_size:int, 
+        name: Optional[str]=None):
 
         super().__init__(name=name)
         self.num_heads = num_heads
@@ -96,35 +94,76 @@ class TransformerLayer(hk.Module):
 class LearnedPositionalEmbeddings(hk.Module):
     """Position embeddings to be added to token embeddings"""
     def __init__(self, 
-                vocab_size: int,
-                embed_dim: int,
-                padding_idx: Optional[int] = 1,
-                name: Optional[str] = None):
+        vocab_size: int,
+        embed_dim: int,
+        padding_idx: Optional[int] = 1,
+        name: Optional[str] = None):
 
         super().__init__(name=name)
         self.vocab_size = vocab_size
         self.embed_dim = embed_dim
         self.padding_idx = padding_idx
     
-    def __call__(self, tokens: jnp.ndarray) -> jnp.ndarray:
+    def __call__(self, 
+        tokens: jnp.ndarray
+        ) -> jnp.ndarray:
         mask = tokens != self.padding_idx
         positions = jnp.cumsum(mask, axis=1) * mask + self.padding_idx
 
         return hk.Embed(self.vocab_size + self.padding_idx + 1, self.embed_dim)(positions)
 
+class ContactPredHead(hk.Module):
+    def __init__(self,
+        padding_idx: Optional[int] = 1,
+        eos_idx: Optional[int] = 2,
+        name: Optional[str] = None):
+
+        self.padding_idx = padding_idx
+        self.eos_idx = eos_idx
+        super().__init__(name=name)
+
+    def __call__(self, 
+        tokens: jnp.ndarray,
+        head_weights: jnp.ndarray
+        ) -> jnp.ndarray:
+
+        head_weights = self.avg_prod_correct(self.symmetrize(head_weights))
+        B, layers, heads, T, _ = head_weights.shape
+        head_weights = head_weights.reshape(B, layers*heads, T, T) 
+        head_weights = jnp.transpose(head_weights, axes=(0, 2, 3, 1))
+        
+        return jax.nn.sigmoid(hk.Linear(1, name="regression")(head_weights))[:, :, :, 0]
+
+    @staticmethod
+    def avg_prod_correct(x):
+        a1 = x.sum(axis=-1, keepdims=True)
+        a2 = x.sum(axis=-2, keepdims=True)
+
+        a12 = x.sum(axis=(-1, -2), keepdims=True)
+
+        avg = (a1 * a2) / a12 
+        normalized = x - avg 
+
+        return normalized
+    
+    @staticmethod
+    def symmetrize(x):
+        # symmetrize along token x token dimensions
+        return x + jnp.transpose(x, axes=(0, 1, 2, 4, 3))
+
 class ESM1b(hk.Module):
     def __init__(self,
-                padding_idx: Optional[int] = 1,
-                head_weights: Optional[bool] = False,
-                name: Optional[str] = None):
+        padding_idx: Optional[int] = 1,
+        head_weights: Optional[bool] = False,
+        name: Optional[str] = None):
         
         self.padding_idx = padding_idx
         self.head_weights = head_weights
         super().__init__(name=name)
     
     def __call__(self, 
-                tokens: jnp.ndarray
-                ) -> "dict[str, jnp.ndarray]":
+        tokens: jnp.ndarray
+        ) -> "dict[str, jnp.ndarray]":
         padding_mask = tokens == self.padding_idx
 
         att_mask = ~padding_mask[:, None, :]
@@ -147,8 +186,27 @@ class ESM1b(hk.Module):
                 all_attn_weights.append(output["attn_weights"])
 
         if self.head_weights:
-            all_attn_weights = jnp.stack(all_attn_weights)
+            all_attn_weights = jnp.stack(all_attn_weights, axis=1)
             
         x = hk.LayerNorm(axis=-1, create_scale=True, create_offset=True, name="emb_layer_norm_after")(x)
         
-        return {"embeds": x, "head_weights": all_attn_weights}
+        return {"embeddings": x, "head_weights": all_attn_weights}
+
+class ESM1bContactPredictor(hk.Module):
+    def __init__(self,
+        padding_idx: Optional[int] = 1,
+        eos_idx: Optional[int] = 2,
+        name: Optional[str] = None
+        ):
+
+        self.padding_idx = padding_idx
+        self.eos_idx = eos_idx
+        super().__init__(name=name)
+    
+    @hk.transparent
+    def __call__(self, 
+        tokens: jnp.ndarray
+        ) -> jnp.ndarray:
+        
+        output = ESM1b(head_weights=True, name="esm1b")(tokens)
+        return ContactPredHead()(output["embeddings"], output["head_weights"])
